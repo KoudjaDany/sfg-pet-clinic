8965f909b79942345e4c6b44fa435d1e38338efc
Dany Koudja <koudjadany@gmail.com>
2020-01-08 14:31:48 +0100

    Initial Project Add

---
+++ src/test/java/training/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package training.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
