00136d41313224fd44e53c16ac1772c86c578f4c
Dany Koudja <koudjadany@gmail.com>
2020-01-22 15:34:41 +0100

    Refactoring interfaces into common crudInterface. Closes #11.

--- pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/services/CrudService.java
@@ -6,10 +6,14 @@ import java.util.Set;
  *
  * @param <E> the Entity type to manage
  */
-public interface CrudService<E> {
-    E findById(Long id);
+public interface CrudService<E, ID> {
+    E findById(ID id);
 
     E save(E entity);
 
     Set<E> findAll();
+
+    void delete(E entity);
+
+    void deleteById(ID id);
 }
--- pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package training.springframework.sfgpetclinic.services;
 
 import training.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
