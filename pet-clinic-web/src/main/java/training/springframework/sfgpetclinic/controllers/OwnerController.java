83440f1cf29b3e32754e8b9afaab1e2aec772594
Dany Koudja <koudjadany@gmail.com>
2020-01-24 15:27:45 +0100

    List all Vets on the owners index page. closes #15

--- pet-clinic-web/src/main/java/training/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/training/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -16,7 +16,7 @@ public class OwnerController {
     }
 
     @RequestMapping({"/index", "/index.html", "/",""})
-    public String index(Model model){
+    public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
--- pet-clinic-web/src/main/java/training/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/training/springframework/sfgpetclinic/controllers/VetController.java
@@ -4,14 +4,21 @@ package training.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import training.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/index", "/index.html", "/",""})
-    public String index(Model model){
+    private VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/index", "/index.html", "/",""})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
