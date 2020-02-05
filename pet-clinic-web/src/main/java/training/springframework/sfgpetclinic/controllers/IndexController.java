67a8ec2182bde53f542e0a3a23315fc1f752c135
Dany Koudja <koudjadany@gmail.com>
2020-02-05 13:53:58 +0100

    Fix broken links.. closes #45

--- pet-clinic-web/src/main/java/training/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/training/springframework/sfgpetclinic/controllers/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping({"/oups", "/find"})
+    public String error(){
+        return "not-implemented";
+    }
 }
