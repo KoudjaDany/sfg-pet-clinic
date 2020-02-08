58d5977ea292f26c7ce2d6864b001aef69319b31
Dany Koudja <koudjadany@gmail.com>
2020-02-08 09:21:15 +0100

    Create Spring Data JPA service for Pet. closes #45

---
+++ pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/services/springdatajpa/PetSpringDataJpaService.java
@@ -0,0 +1,49 @@
+package training.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import training.springframework.sfgpetclinic.model.Pet;
+import training.springframework.sfgpetclinic.repositories.PetRepository;
+import training.springframework.sfgpetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Service
+@Profile("sprindatajpa")
+public class PetSpringDataJpaService implements PetService {
+
+    private PetRepository petRepository;
+
+    public PetSpringDataJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet entity) {
+        return petRepository.save(entity);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet entity) {
+        petRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
