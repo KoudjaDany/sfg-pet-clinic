037a8feed3a020bc1ba06a0cf309762d48bd7e03
Dany Koudja <koudjadany@gmail.com>
2020-02-08 08:10:58 +0100

    Convert Visit to JPA Entity and complete mapping with Pet. closes #38

--- pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/training/springframework/sfgpetclinic/model/Visit.java
@@ -2,15 +2,22 @@ package training.springframework.sfgpetclinic.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
-    @DateTimeFormat
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "date")
     private LocalDate date;
 
+    @Column(name = "description")
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
